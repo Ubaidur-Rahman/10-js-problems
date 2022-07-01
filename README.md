117bb9f2c9d8b538d13a0040e941eafe066b5e76
Ubaidur-Rahman <ubaidurrahmanmcc@gmail.com>
2022-07-01 16:47:52 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-lpakht
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-lpakht)
---
+++ index.js
@@ -0,0 +1,19 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+// problem 1
+// Write a function that accepts an array of strings. Return the longest string.
+
+function longestString(arr) {
+  let longest = '';
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length > longest.length) {
+      longest = arr[i];
+    }
+  }
+  return longest;
+}
