a3161259061bc18f0733c6d76d333398d7ea2628
Ubaidur-Rahman <ubaidurrahmanmcc@gmail.com>
2022-07-31 01:52:10 +0600

    167. Two Sum II - Input Array Is Sorted solved using google help

--- index.js
+++ index.js
@@ -539,4 +539,25 @@ var twoSum = function (nums, target) {
   }
 };
 
-console.log(twoSum([3, 2, 4], 7));
+// console.log(twoSum([3, 2, 4], 7));
+
+// Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+
+// Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+
+// The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+// Your solution must use only constant extra space.
+
+var twoSumAdvance = function (numbers, target) {
+  let result = new Map();
+  for (let i = 0; i <= numbers.length; i++) {
+    if (result.get(target - numbers[i]) !== undefined) {
+      return [result.get(target - numbers[i]) + 1, i + 1];
+    } else {
+      result.set(numbers[i], i);
+    }
+  }
+};
+
+// console.log(twoSumAdvance([3, 2, 4], 7));
