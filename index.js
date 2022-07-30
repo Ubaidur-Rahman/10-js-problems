2a9710e8d3b2dc5c7a1b02cdbc2881ca01a5590a
Ubaidur-Rahman <ubaidurrahmanmcc@gmail.com>
2022-07-31 01:28:27 +0600

    twosum problem solved

--- index.js
+++ index.js
@@ -511,16 +511,32 @@ var runningSum = function (nums) {
 
 // console.log(runningSum([2, 5, 1, 3, 4, 7]))
 
-var findMiddleIndex = function(nums) {
+var findMiddleIndex = function (nums) {
   let postSum = nums.reduce((a, b) => a + b);
-  console.log(postSum)
+  console.log(postSum);
   let preSum = 0;
   for (let i = 0; i < nums.length; i++) {
-      postSum -= nums[i];
-      if (postSum === preSum) return i;
-      preSum += nums[i]
+    postSum -= nums[i];
+    if (postSum === preSum) return i;
+    preSum += nums[i];
   }
   return -1;
-}
+};
 
-console.log(findMiddleIndex([1,5,4,6]))
+// console.log(findMiddleIndex([1,5,4,6]))
+
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] == target) return [i, j];
+    }
+  }
+};
+
+console.log(twoSum([3, 2, 4], 7));
