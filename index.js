00ffdc472dbe6d611070a67325101cce6ed773ed
Ubaidur-Rahman <ubaidurrahmanmcc@gmail.com>
2022-08-02 20:38:36 +0600

    Implement strStr() solved

--- index.js
+++ index.js
@@ -722,3 +722,22 @@ console.log(isPowerOfThree(27));
 // };
 
 // console.log(removeElement([3, 2, 2, 3], 3));
+
+// Implement strStr().
+
+// Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+// Clarification:
+
+// What should we return when needle is an empty string? This is a great question to ask during an interview.
+
+// For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
+
+var strStr = function (haystack, needle) {
+  if (haystack.indexOf(needle) === -1) {
+    return -1;
+  }
+  return haystack.indexOf(needle);
+};
+
+console.log(strStr('a', 'a'));
