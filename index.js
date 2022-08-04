95d173199375322878b116273be2ff21b1b11fad
Ubaidur-Rahman <ubaidurrahmanmcc@gmail.com>
2022-08-04 07:33:36 +0600

    merge arr solved but don't pass testcase

--- index.js
+++ index.js
@@ -812,3 +812,24 @@ var singleNumber = function (nums) {
   }
   return ans;
 };
+
+
+
+// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+// Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+// The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+
+var merge = function(nums1, m, nums2, n) {
+    let removeZero = arr=>arr.filter(el=>!(el===0))
+   let n1 = removeZero(nums1)
+   let n2 = removeZero(nums2)
+  nums1 = [...n1, ...n2].sort((a,b)=>a-b)
+  return nums1
+};
+
+console.log(merge([0],0,[2,5,6],3))
+
+
