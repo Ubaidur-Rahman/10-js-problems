2d8dee271f008e7f4f06a6e759db90f9b289f0db
Ubaidur-Rahman <ubaidurrahmanmcc@gmail.com>
2022-08-06 16:24:19 +0600

    reformat number using regX

--- index.js
+++ index.js
@@ -843,4 +843,23 @@ var uniqueOccurrences = function (arr) {
   return false;
 };
 
-console.log(uniqueOccurrences([-3, 0, 1, -3, 1, 1, 1, -3, 10, 0]));
+// console.log(uniqueOccurrences([-3, 0, 1, -3, 1, 1, 1, -3, 10, 0]));
+
+// You are given a phone number as a string number. number consists of digits, spaces ' ', and/or dashes '-'.
+
+// You would like to reformat the phone number in a certain manner. Firstly, remove all spaces and dashes. Then, group the digits from left to right into blocks of length 3 until there are 4 or fewer digits. The final digits are then grouped as follows:
+
+// 2 digits: A single block of length 2.
+// 3 digits: A single block of length 3.
+// 4 digits: Two blocks of length 2 each.
+// The blocks are then joined by dashes. Notice that the reformatting process should never produce any blocks of length 1 and produce at most two blocks of length 2.
+
+// Return the phone number after formatting.
+
+var reformatNumber = function (number) {
+  const reRemoveDash = /\D/g;
+  const reSeparateByDash = /(...?(?=..))/g;
+  return number.replace(reRemoveDash, '').replace(reSeparateByDash, '$1-');
+};
+
+// console.log(reformatNumber('1-23-45 6 4'));
