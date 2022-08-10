cfe9ba27a4a63f1f8092db95f6b6e3754afacb09
Ubaidur-Rahman <ubaidurrahmanmcc@gmail.com>
2022-08-10 22:33:07 +0600

    ugly number

--- index.js
+++ index.js
@@ -942,3 +942,14 @@ console.log(reverseString(['H', 'a', 'n', 'n', 'a', 'h']));
 var sum = function (num1, num2) {
   return num1 + num2;
 };
+
+// An ugly number is a positive integer whose prime factors are limited to 2, 3, and 5.
+
+// Given an integer n, return true if n is an ugly number.
+
+var isUgly = function (n) {
+  if (n % 2 === 0 || n % 5 === 0 || n % 5 === 0) {
+    return true;
+  }
+  return false;
+};
