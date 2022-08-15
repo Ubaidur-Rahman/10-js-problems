0632ffcdbe5076d0b70b8b4b96a24553a58f936e
Ubaidur-Rahman <ubaidurrahmanmcc@gmail.com>
2022-08-15 19:46:32 +0600

    get concatination solved

--- index.js
+++ index.js
@@ -1010,4 +1010,15 @@ var countOdds = function (low, high) {
   return odd;
 };
 
-console.log(countOdds(800445804, 979430543));
+// console.log(countOdds(800445804, 979430543));
+
+// Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+
+// Specifically, ans is the concatenation of two nums arrays.
+
+// Return the array ans.
+
+var getConcatenation = function (nums) {
+  return [...nums, ...nums];
+};
+console.log(getConcatenation([1, 2, 1]));
