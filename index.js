6a7fca367666276797d27e05baf63e4c5d69cc9f
Ubaidur-Rahman <ubaidurrahmanmcc@gmail.com>
2022-08-19 22:38:13 +0600

    Merge 2 list array

--- index.js
+++ index.js
@@ -1273,3 +1273,10 @@ var findSpecialInteger = function (arr) {
 };
 
 console.log(findSpecialInteger([1, 1, 2, 3, 3, 4, 4, 6, 6, 6]));
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+
