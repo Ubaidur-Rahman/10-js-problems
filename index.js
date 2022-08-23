2b7272ff369a83fe1936396cddf07a1d6633b75e
Ubaidur-Rahman <ubaidurrahmanmcc@gmail.com>
2022-08-23 19:31:06 +0600

    number of Match solved

--- index.js
+++ index.js
@@ -1313,4 +1313,26 @@ var arrayStringsAreEqual = function (word1, word2) {
   return word1.join('') === word2.join('');
 };
 
-console.log(arrayStringsAreEqual(['ab', 'c'], ['abc']));
+// console.log(arrayStringsAreEqual(['ab', 'c'], ['abc']));
+
+// You are given an integer n, the number of teams in a tournament that has strange rules:
+
+// If the current number of teams is even, each team gets paired with another team. A total of n / 2 matches are played, and n / 2 teams advance to the next round.
+// If the current number of teams is odd, one team randomly advances in the tournament, and the rest gets paired. A total of (n - 1) / 2 matches are played, and (n - 1) / 2 + 1 teams advance to the next round.
+// Return the number of matches played in the tournament until a winner is decided.
+
+var numberOfMatches = function (n) {
+  let totalMatch = 0;
+  while (n > 1) {
+    if (n % 2 === 0) {
+      totalMatch += n / 2;
+      n = n / 2;
+    } else {
+      totalMatch += (n - 1) / 2;
+      n = (n - 1) / 2 + 1;
+    }
+  }
+  return totalMatch;
+};
+
+console.log(numberOfMatches(5));
